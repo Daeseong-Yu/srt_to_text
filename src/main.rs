7e96f04347e75b2c009ac5634c2ac9ecd3b815f3
Daeseong Yu <dyu8422@conestogac.on.ca>
2026-02-18 19:44:38 -0500

    Extract ony Englich sentences and combine into single line

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::env;
 use std::process;
+use std::fs;
 
 fn main() {
     let mut args = env::args();
-    let _exe = args.next();     // exxcutable filename
+    let _exe = args.next();     // Executable filename
 
     let filename = match args.next() {
         Some(filename) => filename,
@@ -14,4 +15,30 @@ fn main() {
     };
 
     println!("input filename: {}", filename);
+
+    // Read all bytes until EOF
+    let contents = match fs::read_to_string(&filename) {
+        Ok(text) => text,
+        Err(err) => {
+            eprintln!("Failed to read {}: {}", filename, err);
+            process::exit(1);
+        }
+    };
+
+    // println!("contents: {contents}");
+    let mut texts = Vec::new();
+
+    for raw_line in contents.lines() {
+        let line = raw_line.trim();
+
+        let number_symbol: bool = line.chars().all(|c| c.is_ascii_digit());
+
+        if !line.is_empty() && !line.contains("-->") && !number_symbol {
+            texts.push(line.to_string());
+        }
+    }
+
+    let result = texts.join(" ");
+
+    println!("{result}");
 }
